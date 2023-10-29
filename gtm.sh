7588242f9bbe6a1f51bfe3ed8e89c8a9c43d3afd
NewLegends12 <148504272+NewLegends12@users.noreply.github.com>
2023-10-29 21:30:50 +0800

    Update gtm.sh

--- gtm.sh
+++ gtm.sh
@@ -2,7 +2,7 @@ A='myudp.elcavlaw.com'
 NS='sdns.myudp.elcavlaw.com'
 LOOP_DELAY=5
 
-declare -a HOSTS=('112.198.115.44' '124.6.181.20')
+declare -a HOSTS=('112.198.115.44' '124.6.181.67' '124.6.181.20')
 DIG_EXEC="DEFAULT"
 
 CUSTOM_DIG=/data/data/com.termux/files/home/go/bin/fastdig
