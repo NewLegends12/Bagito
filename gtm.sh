e335e68f10cfcb0b3f5f4b8244650dd9f30da4ce
NewLegends12 <148504272+NewLegends12@users.noreply.github.com>
2023-11-21 21:27:07 +0800

    Update gtm.sh

--- gtm.sh
+++ gtm.sh
@@ -11,6 +11,14 @@ NS1='ns-artph.elcavlaw.com'
 A1='artph.elcavlaw.com'
 NS2='sdns.myudp1.elcavlaw.com'
 A2='myudp1.elcavlaw.com'
+NS3='sdns.myudph.elcavlaw.com'
+A3='myudp1.elcavlaw.com'
+NS4='ns-artph1.elcavlaw.com'
+A4='artph1.elcavlaw.com'
+NS5='ns-artsg1.elcavlaw.com'
+A5='artsg1.elcavlaw.com'
+NS6='ns-artsg2.elcavlaw.com'
+A6='artsg2.elcavlaw.com'
 
 ## Repeat dig cmd loop time (seconds) (positive interger only)
 LOOP_DELAY=0
@@ -44,13 +52,13 @@ if [ ! $(command -v ${_DIG}) ]; then
  "\$DIG_EXEC & \$CUSTOM_DIG variable inside $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename "$0") file.\n" && exit 1
 fi
 endscript() {
- unset NS A  NS1 A1 NS2 A2 LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
+ unset NS A NS1 A1 NS2 A2 NS3 A3 NS4 A4 NS5 A5 NS6 A6 LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
  exit 1
 }
 trap endscript 2 15
 check(){
  for ((i=0; i<"${#HOSTS[*]}"; i++)); do
-  for R in "${A}" "${NS}" "${A1}" "${NS1}" "${A2}" "${NS2}"; do
+  for R in "${NS}" "${A}" "${NS1}" "${A1}" "${NS2}" "${A2}" "${NS3}" "${A3}" "${NS4}" "${A4}" "${NS5}" "${A5}" "${NS6}" "${A6}"; do
    T="${HOSTS[$i]}"
    [[ -z $(timeout -k 3 3 ${_DIG} @${T} ${R}) ]] && M=31 || M=32;
    echo -e "\e[1;${M}m\$ R:${R} D:${T}\e[0m"
