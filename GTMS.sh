c00f17809931f5bdcaa56b63bad1d3a1c77419a2
NewLegends12 <148504272+NewLegends12@users.noreply.github.com>
2023-11-27 22:44:38 +0800

    Update GTMS.sh

--- GTMS.sh
+++ GTMS.sh
@@ -1,84 +1,44 @@
 #!/bin/bash
 
-# Your DNSTT Nameserver & your Domain `A` Record
-declare -a NS=('sdns.myudp.elcavlaw.com' 'sdns.myudp1.elcavlaw.com' 'sdns.myudph.elcavlaw.com' 'ns-sgfree.elcavlaw.com')
-declare -a A=('myudp.elcavlaw.com' 'myudp1.elcavlaw.com' 'myudph.elcavlaw.com' 'sgfree.elcavlaw.com')
+# List of DNS servers to query
+declare -a DNS_SERVERS=("8.8.8.8" "8.8.4.4" "208.67.222.222" "208.67.220.220")
 
-# Repeat dig cmd loop time (seconds) (positive integer only)
-LOOP_DELAY=5
-
-# Add your DNS IP addresses here
-declare -a HOSTS=('124.6.181.12' '124.6.181.36')
+# List of domains to check
+declare -a DOMAINS=("vt.tiktok.com" "new.globe.com.ph" "app1.smart.com.ph")
 
 # Linux' dig command executable filepath
-# Select value: "CUSTOM|C" or "DEFAULT|D"
-DIG_EXEC="DEFAULT"
-# If set to CUSTOM, enter your custom dig executable path here
-CUSTOM_DIG="/usr/bin/dig"
-
-# Log file path
-LOG_FILE="dns_hunter_log.txt"
-
-# Verify dig command availability
-if [ "$DIG_EXEC" == "DEFAULT" ]; then
-  _DIG="$(command -v dig)"
-else
-  _DIG="$CUSTOM_DIG"
-fi
+DIG_EXEC="/usr/bin/dig"
 
 # Check if dig is installed
-if [ -z "$_DIG" ]; then
-  echo "Error: 'dig' command not found. Install dnsutils or set a valid custom dig executable path."
+if [ ! -x "$DIG_EXEC" ]; then
+  echo "Error: 'dig' command not found. Install dnsutils or set a valid dig executable path."
   exit 1
 fi
 
-dns_hunter() {
-  echo "DNS Hunter: Searching for leaked IPs..."
-  
-  # Iterate through each DNS and IP combination
-  for ((i=0; i<"${#HOSTS[*]}"; i++)); do
-    for R in "${NS[@]}" "${A[@]}"; do
-      T="${HOSTS[$i]}"
-      
-      # Run a command to check if the DNS resolution matches an expected result
-      result=$(${_DIG} "@${T}" "${R}" +short 2>/dev/null)
-      
-      # Define your logic here to identify potential issues
-      if [ "${result}" != "" ] && [ "${result}" != "EXPECTED_IP" ]; then
-        echo "Potential issue detected: DNS ${T} resolved to unexpected IP ${result}, NameServer: ${R}"
-        
-        # Log the event with timestamp and details
-        log_result "${T}" "${R}" "${result}" "Potential issue detected"
-        
-        # Add further actions or logging as needed
-        # For example, send an alert, block the IP, etc.
+hunt_dns() {
+  local border_color="\e[95m"  # Light magenta color
+  local success_color="\e[92m"  # Light green color
+  local fail_color="\e[91m"    # Light red color
+  local reset_color="\e[0m"    # Reset to default terminal color
+  local padding="  "            # Padding for aesthetic
+
+  for DOMAIN in "${DOMAINS[@]}"; do
+    for DNS_SERVER in "${DNS_SERVERS[@]}"; do
+      result=$("$DIG_EXEC" +short "@$DNS_SERVER" "$DOMAIN")
+
+      if [ -z "$result" ]; then
+        STATUS="${success_color}Not Found${reset_color}"
+      else
+        STATUS="${fail_color}Found${reset_color}"
       fi
+
+      echo -e "${border_color}Domain: $DOMAIN${reset_color}"
+      echo -e "${border_color}DNS Server: $DNS_SERVER${reset_color}"
+      echo -e "${border_color}Status: $STATUS${reset_color}"
+      echo -e "${border_color}Result: $result${reset_color}"
+      echo "----------------------------------------"
     done
   done
-  
-  echo "DNS Hunter: Finished"
 }
 
-log_result() {
-  local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-  echo "${timestamp} - DNS IP: $1, NameServer: $2, Result: $3, Status: $4" >> "$LOG_FILE"
-}
-
-countdown() {
-  for i in {3..1}; do
-    echo "Checking started in $i seconds..."
-    sleep 1
-  done
-}
-
-echo ""
-echo "Begin...."
-echo ""
-countdown
-clear
-
-# Main loop
-while true; do
-  dns_hunter
-  sleep $LOOP_DELAY
-done
+hunt_dns
