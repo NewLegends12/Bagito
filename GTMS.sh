ce604c8daa73f50c0a180272e3e64cbc6039c21a
NewLegends12 <148504272+NewLegends12@users.noreply.github.com>
2023-11-27 13:28:10 +0800

    Update GTMS.sh

--- GTMS.sh
+++ GTMS.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 # Your DNSTT Nameserver & your Domain `A` Record
-NS='sdns.myudp.elcavlaw.com'
-A='myudp.elcavlaw.com'
-NS1='sdns.myudph.elcavlaw.com'
-A1='myudph.elcavlaw.com'
-NS2='sdns.myudp1.elcavlaw.com'
-A2='myudp1.elcavlaw.com'
-NS3='ns-sgfree.elcavlaw.com'
-A3='sgfree.elcavlaw.com'
+DNSTT_SERVERS=(
+  'sdns.myudp.elcavlaw.com:myudp.elcavlaw.com'
+  'sdns.myudph.elcavlaw.com:myudph.elcavlaw.com'
+  'sdns.myudp1.elcavlaw.com:myudp1.elcavlaw.com'
+  'ns-sgfree.elcavlaw.com:sgfree.elcavlaw.com'
+)
 
 # Repeat dig cmd loop time (seconds) (positive integer only)
 LOOP_DELAY=1
 
 # Add your DNS here
-declare -a HOSTS=('124.6.181.12' '124.6.181.36')
+TARGET_DNS=('124.6.181.12' '124.6.181.36')
 
 # Linux' dig command executable filepath
 _DIG="$(command -v dig)"
@@ -25,27 +23,31 @@ if [ -z "${_DIG}" ]; then
 fi
 
 endscript() {
-  unset NS A NS1 A1 NS2 A2 NS3 A3 LOOP_DELAY HOSTS _DIG
+  unset DNSTT_SERVERS LOOP_DELAY TARGET_DNS _DIG
   exit 1
 }
 
 trap endscript 2 15
 
 check() {
-  for T in "${HOSTS[@]}"; do
-    for R in "${A}" "${NS}" "${A1}" "${NS1}" "${A2}" "${NS2}" "${A3}" "${NS3}"; do
-      IP="$(${_DIG} +short "@${T}" "${R}" | tail -n 1)"
+  for DNS_PAIR in "${DNSTT_SERVERS[@]}"; do
+    IFS=':' read -ra DNS <<< "${DNS_PAIR}"
+    NS="${DNS[0]}"
+    A="${DNS[1]}"
+
+    for TARGET in "${TARGET_DNS[@]}"; do
+      IP="$(${_DIG} +short "@${TARGET}" "${A}" | tail -n 1)"
       if [ -n "${IP}" ]; then
-        echo "Success: R:${R} D:${T} IP:${IP}"
+        echo "Success: NS:${NS} A:${A} TARGET:${TARGET} IP:${IP}"
       else
-        echo "Error: Failed to get IP for R:${R} D:${T}"
+        echo "Error: Failed to get IP for NS:${NS} A:${A} TARGET:${TARGET}"
       fi
     done
   done
 }
 
 echo "DNSTT Keep-Alive script <Lantin Nohanih>"
-echo "DNS List: [${HOSTS[*]}]"
+echo "DNS List: [${TARGET_DNS[*]}]"
 echo "CTRL + C to close script"
 
 [[ "${LOOP_DELAY}" -eq 1 ]] && let "LOOP_DELAY++";
